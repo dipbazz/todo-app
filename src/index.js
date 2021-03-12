c9a6f6156ff46b97f741c638f067846f47dbf39c
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-12 19:20:53 +0500

    add loadProject function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+import 'bootstrap';
+import 'jquery';
+import $ from 'jquery'
 import { getProjects, addProject } from './modules/projects';
 import {project_list} from './dom/projects'
 
-document.getElementById('projects').innerHTML = getProjects().map( project => {
-  return project_list(project.title);
-}).join('');
+
+const loadProject = () => {
+  return document.getElementById('projects').innerHTML = getProjects().map( project => {
+    return project_list(project.title);
+  }).join('');
+}
+
+loadProject()
 
 document.getElementById('add-project').addEventListener('click', (e) => {
   e.preventDefault();
   const { project } = e.target.form;
   console.log(addProject(project.value));
+  e.target.form.reset()
+  $('#projectModal').modal('hide')
+  loadProject()
 })
+
