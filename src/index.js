0a42be3a425969e1cf0947bccbdd4495894239f8
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-15 18:42:19 +0500

    update index js with MVC pattern

--- src/index.js
+++ src/index.js
@@ -1,31 +1,57 @@
-import 'bootstrap';
-import 'jquery';
+import 'bootstrap'
+import 'jquery'
+import  Projects  from './model/projects';
+import projectsController from './controller/projectsController'
+import projectsView from "./view/projectsView";
 
-import { getProjects, addProject } from './modules/projects';
-import { projectList } from './dom/projects';
-import { todoContainer } from './dom/todo'
+const model = new Projects();
+const view = new projectsView()
+const controller = new projectsController(model, view);
 
-
-const loadProject = () => {
-  document.getElementById('projects').innerHTML = getProjects().map(project => projectList(project.title)).join('');
-};
-
-loadProject();
-
-document.getElementById('add-project').addEventListener('click', (e) => {
-  e.preventDefault();
-  const { project } = e.target.form;
-  addProject(project.value);
-  e.target.form.reset();
-  $('#projectModel').modal('hide'); // eslint-disable-line
-  loadProject();
-});
+controller.showProjects()
 
 
-document.querySelectorAll('.project-list-item').forEach(item => {
-  item.addEventListener('click', (e) => {
-    const project = e.target.innerHTML;
 
-    document.getElementById('task-container').innerHTML = todoContainer(project);
-  })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'bootstrap';
+// import 'jquery';
+
+// import projectController from './controller/projectsController'
+// import { projectList } from './dom/projects';
+// import { todoContainer } from './dom/todo'
+
+
+// const loadProject = () => {
+//   document.getElementById('projects').innerHTML = getProjects().map(project => projectList(project.title)).join('');
+// };
+
+// loadProject();
+
+// document.getElementById('add-project').addEventListener('click', (e) => {
+//   e.preventDefault();
+//   const { project } = e.target.form;
+//   addProject(project.value);
+//   e.target.form.reset();
+//   $('#projectModel').modal('hide'); // eslint-disable-line
+//   loadProject();
+// });
+
+
+// document.querySelectorAll('.project-list-item').forEach(item => {
+//   item.addEventListener('click', (e) => {
+//     const project = e.target.innerHTML;
+
+//     document.getElementById('task-container').innerHTML = todoContainer(project);
+//   })
+// })
