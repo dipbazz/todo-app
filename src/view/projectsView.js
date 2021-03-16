a68e7352cc1379d1a01f28bb88ae8a7f8e0fd23a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-16 14:02:19 +0500

    fix indentation erorr

--- src/view/projectsView.js
+++ src/view/projectsView.js
@@ -1,27 +1,19 @@
 class projectsView {
-    constructor() {
-        this.projects = document.createElement('ul')
-        this.projects.classList.add(...["list-group", "list-group-flush", "mb-4"])
-    }
+  constructor() {
+    this.projects = document.createElement('ul');
+    this.projects.classList.add(...['list-group', 'list-group-flush', 'mb-4']);
+  }
 
 
-    render(projects) {
-        projects.forEach((project) => {
-           const listItem = document.createElement('li')
-           listItem.classList.add('list-group-item')
-           listItem.innerHTML = project.title
-           this.projects.append(listItem)
-        })
-
-        document.querySelector('aside').append(this.projects)
-    }
-
-
-    .addEventListener('click', (e) => {
-        e.preventDefault()
-        console.log(e.target)
-    })
-
+  render(projects) {
+    projects.forEach((project) => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('list-group-item');
+      listItem.innerHTML = project.title;
+      this.projects.append(listItem);
+    });
+    document.querySelector('aside').append(this.projects);
+  }
 }
 
-export default projectsView
+export default projectsView;
