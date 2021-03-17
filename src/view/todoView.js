7bb56153698e51428bf21f36b70c197c65f9cdab
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-17 16:05:55 +0500

    add todo modal view

--- src/view/todoView.js
+++ src/view/todoView.js
@@ -1,15 +1,19 @@
 const todoView = () => {
-    const render = (project, todos) => {
+  const taskDescription = document.querySelector('.task-description')
+  const taskDate = document.querySelector('.task-date')
+  const taskVolume = document.querySelector('.task-volume')
+  const taskTitle = document.querySelector('.task-title')
+    const render = (project_id, project, todos) => {
       console.log(project, todos, '###3');
       const element = `
-      <h1 class="text-center">${project.title}</h1>
+      <h1 class="text-center" data-project-index="${project_id}" id="project-title">${project.title}</h1>
       <div  class="d-flex justify-content-end mb-2">
         <button type="button" class="btn btn-success" data-toggle="modal" data-target="#todoModal">
           Add Task
         </button>
       </div>
       <ul class="list-group list-group-flush mb-4">
-      ${todos.map((todo, index) => {
+      ${todos.map((todo) => {
         return `
         <li class="task-list-item mb-3 list-group-item list-group-item-action border-${todo.priority == 'high' ? 'danger' : todo.priority == 'medium' ? 'warning' : 'info'} border-left border-bottom-0">
             <div class="d-flex align-items-center">
@@ -30,7 +34,37 @@ const todoView = () => {
       document.getElementById('todos').innerHTML = element;
     }
 
-    return { render };
+    const detailTemplate = (todo) => {
+      console.log(todo,'213123')
+      const element = `
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title task-title" id="detailModelLabel">${todo.title}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+              <p class="task-description">
+              ${todo.description}
+              </p>
+              <div class="d-flex justify-content-between">
+                <span>Due date: ${todo.date}</span>
+                <span class='task-volume'>${todo.priority}</span>
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-outline-success">Edit</button>
+            <button type="submit" class="btn btn-outline-danger">Delete</button>
+          </div>
+        </div>
+      </div>
+      `;
+      document.getElementById('detailModel1').innerHTML = element;
+    }
+
+    return { render, detailTemplate };
 }
 
 export default todoView;
