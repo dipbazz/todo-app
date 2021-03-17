71ea40d334bf6e1bdf8a5d0721efb29b883eaa5e
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-17 16:07:41 +0500

    add project_id to todo

--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -2,13 +2,14 @@ const todoController = (todoModel, todoView) => {
     let model = todoModel();
     let view = todoView();
 
-    const addTodo = (todo,priority,dueDate,description) => {
+    const addTodo = (todo,priority,dueDate,description,project_id) => {
       const new_todo = model.todo(todo, priority, dueDate, description)
-      model.save(new_todo);
+      model.save(new_todo, project_id);
+      console.log(project_id,'cccc')
     }
 
     const showTodos = (project_id = 0) => {
-      view.render(model.getProject(project_id), model.getProject(project_id).tasks);
+      view.render(project_id,model.getProject(project_id), model.all(project_id));
     }
 
 
