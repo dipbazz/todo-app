f4c2f27166a2fc165e8b0611608a82ab4dfe77f6
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-17 21:03:49 +0500

    add edit method controller

--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -2,9 +2,14 @@ const todoController = (todoModel, todoView) => {
     let model = todoModel();
     let view = todoView();
 
-    const addTodo = (todo,priority,dueDate,description,project_id) => {
-      const new_todo = model.todo(todo, priority, dueDate, description)
-      model.save(new_todo, project_id);
+    const addTodo = (title,priority,dueDate,description,project_id, id = null) => {
+      const new_todo = model.todo(title, priority, dueDate, description)
+      console.log(id)
+      if (id) {
+        model.edit(project_id, id, new_todo)
+      } else {
+        model.save(new_todo, project_id);
+      }
     }
 
     const showTodos = (project_id = 0) => {
@@ -15,8 +20,13 @@ const todoController = (todoModel, todoView) => {
       model.remove(project_id, todo_id);
     }
 
+    const editTodo = (project_id, todo_id) => {
+      view.updateTodoModel(model.get(project_id,todo_id),todo_id)
 
-    return { addTodo, showTodos, removeTodo };
+    }
+
+
+    return { addTodo, showTodos, removeTodo, editTodo };
 }
 
 export default todoController;
